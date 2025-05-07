35dae28fbfc8a1cf5347e1fbf9035f106987fe20
Wu <wuc4@esplhpccompbio-lv03.cm.cluster>
2025-05-07 15:09:18 -0700

    direct inference 25baseline model and save results

--- scripts/CMR2025train_chaowei_directinference.sh
+++ scripts/CMR2025train_chaowei_directinference.sh
@@ -6,17 +6,18 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=200G
 #SBATCH --time=168:00:00
-#SBATCH --gres=gpu:l40s:2
+#SBATCH --gres=gpu:l40s:1
 #SBATCH --mail-user=chaowei.wu@cshs.org
 #SBATCH --mail-type=END,FAIL
 
 source activate cmr
 
-CMRROOT=/common/lidxxlab/cmrchallenge/code/chaowei
-SAVE_DIR=/common/lidxxlab/cmrchallenge/data/CMR2025
+CMRROOT=/common/lidxxlab/chaowei/PromptMR-plus
+SAVE_DIR=$CMRROOT/exp/CMR2025
 
 echo "Detailed GPU Information:"
-nvidia-smi -L
+# nvidia-smi -L
+nvidia-smi
 echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 
 wandb login b893a6d433af71e3cdb71002caa7dc90d57d4587
@@ -28,4 +29,4 @@ python main.py predict \
        -c $CMRROOT/configs/model/pmr-plus.yaml \
        --trainer.devices=auto \
        --model.init_args.pretrain=True \
-       --model.init_args.pretrain_weights_path=$CMRROOT/experiments/cmr25/promptmr-plus/CMR2025/deep_recon/uec2kxvx/checkpoints/last.ckpt
+       --model.init_args.pretrain_weights_path=$CMRROOT/checkpoints/baseline/last.ckpt
