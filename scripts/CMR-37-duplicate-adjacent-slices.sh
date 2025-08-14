92db0e293a3173c57d4235382920ddff1a5acb7f
Junzhou Chen <Junzhou.Chen@cshs.org>
2025-08-14 00:47:12 -0700

    [CMR-40] Implement timepoint duplication (LGE and T2Map) during inference.

--- scripts/CMR-37-duplicate-adjacent-slices.sh
+++ scripts/CMR-37-duplicate-adjacent-slices.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=errors/%j.log
 #SBATCH --partition=gpu
 #SBATCH --cpus-per-task=12
-#SBATCH --mem=200G
+#SBATCH --mem=100G
 #SBATCH --time=24:00:00
 #SBATCH --gres=gpu:l40s:4
 #SBATCH --mail-user=junzhou.chen@cshs.org
