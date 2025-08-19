4597ec559f33924cf737a3f5f1b55637fbcce3d6
Junzhou Chen <Junzhou.Chen@cshs.org>
2025-08-18 17:30:42 -0700

    [CMR-39] update best ckpt and project ID

--- docker_manager.sh
+++ docker_manager.sh
@@ -6,12 +6,12 @@
 set -e
 
 # Default values
-PROJECT_ID="syn59814210"
+PROJECT_ID="syn68754167"
 TAG="latest"
 INPUT_DIR=""
 OUTPUT_DIR=""
 TASK="task-r2"
-CKPT="/common/lidxxlab/cmrchallenge/code/chaowei/experiments/cmr25/promptmr-plus/CMR2025/deep_recon/uec2kxvx/checkpoints/last.ckpt"
+CKPT="$HOME/HPC-LIDXXLAB/Yi/training_results_folder/multi_dataset_training_dataset_specific/promptmr-plus/CMR2024_2025_dataset_specific/cmr2024_2025_phased/1e5n1toc/checkpoints/best-epochepoch=06-valvalidation_loss=0.0150.ckpt"
 # Colors for output
 RED='\033[0;31m'
 GREEN='\033[0;32m'
